46032fb451d6b822f511510c54c5c9ed2f1ed75c
yash10019coder <lit2020066@iiitl.ac.in>
2021-10-28 22:46:17 +0530

    fei

--- node_server/server.js
+++ node_server/server.js
@@ -6,7 +6,8 @@ const path = './SM-project/';
 app.use(express.static(path));
 
 const ip_address = '127.0.0.1';
-const port = process.env.port || 30000;
+// const port = process.env.port || 30000;
+const port = 80;
 
 // console.log('Server running at http://' + ip_address + ':' + port);
 
